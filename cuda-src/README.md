c1e94bffe67e503222ddcae6a92038ed31f34d20
lx <you@example.com>
2021-07-14 08:20:28 +0800

    moved to seperate dir

--- cuda-src/README.md
+++
@@ -1,9 +0,0 @@
-# README
-
-A few simple example of CUDA kernels.
-
-
-Source:
-
-- NVIDIA_CUDA_C_Programming_Guide
-- CUDA Samples
--- cuda-src/vectorAdd/include/vectorAdd.cuh
+++
@@ -1,8 +0,0 @@
-
-// List wrapper function callable by .cpp file.
-void vectorAdd(double *A, double *B, double *C, int arraySize);
-
-
-
-
-
